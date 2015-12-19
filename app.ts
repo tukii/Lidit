c894a60d2e3e3150fe66a2e3ddbcf4bb116c7c4a
Kwoth <srki0509@gmail.com>
2015-12-19 12:33:09 +0100

    index->app, using nodemon, brow-sync with gulp
    -Ignored .js and map files. from root and public/app
    -Moved index to public folder
    -Cleaned up package.jsons
    -Moved to gulp-nodemon and browser-sync
    Live refreshing and all other cool things work with express server now

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 ﻿/// <reference path="typings/socket.io/socket.io.d.ts" />
 /// <reference path="typings/express/express.d.ts" />
+
 import * as express from 'express';
 var app = express();
 var server = require('http').Server(app);
@@ -14,10 +15,10 @@ console.log('Listening on port '+ port);
 //app.use(express.static(__dirname+"/views"));
 app.use('/static',express.static(__dirname+"/public"));
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 app.get('/{lid}', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection',function(socket){
