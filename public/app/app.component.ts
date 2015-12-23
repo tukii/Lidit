82c72dffd945671dfbbff522709beabfaa2f5fad
Kwoth <srki0509@gmail.com>
2015-12-23 23:30:18 +0100

    posting/commenting works until you refresh

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -2,6 +2,9 @@
 import {Component} from 'angular2/core';
 import {AddPostComponent} from './add-post.component.js';
 
+
+var socket = io.connect('http://localhost:8000');
+
 @Component({
     selector: 'lidit-app',
     templateUrl: '/static/views/main.html',
@@ -15,7 +18,7 @@ export class AppComponent {
     socket: any;
 
     constructor() {
-        this.socket = io.connect('http://localhost:8000');
+        this.socket = socket;
 
         this.socket.on('connect',() => {
             console.log('connected'); 
@@ -28,7 +31,7 @@ export class AppComponent {
         this.socket.on('new-comment',(data) => {
             for(var i = 0; i <this.posts.length;i++){
                 if(this.posts[i].id == data.postId){
-                    this.posts[i].comments.push(new Comment(0,0,data.text));
+                    this.posts[i].AddComment(data.text);
                     return;
                 }
             }
@@ -41,12 +44,6 @@ export class AppComponent {
         /*this.AddPost(new Post(1,"Test test 123", []));
         this.AddPost(new Post(2,"Plz say somethign smart", []));
         this.AddPost(new Post(3,"SPAAAAAAAAAAAAAAAAAM",[]));
-        
-        
-        this.AddPost(new Post(0,"BOOM BOOM", [new Comment(5, 2, "Hey guise lel don't go to school tmrw"), new Comment(10, 2, "fake fake fake fake fake fake fake"), new Comment(0, 7, "YEAH RIGHT I DARE U!!!")]));
-        this.AddPost(new Post(1,"Test test 123", [new Comment(5, 2, "Hello worldddddddddddddd")]));
-        this.AddPost(new Post(2,"Plz say somethign smart", [new Comment(5, 2, "l2spell newb")]));
-        this.AddPost(new Post(3,"SPAAAAAAAAAAAAAAAAAM", [new Comment(5, 2, "AAAAAAAAAAAAAAAAAAAAAAAMmmmmmm mmmmmmmmm aaaaaaaaa mmmmmmmmmmmmmb")]));
         */
         
         this.channels = [new Channel("random","b"), new Channel("anime","a"),new Channel("music","m"),new Channel("random","b"), new Channel("anime","a"),new Channel("music","m"),new Channel("random","b"), new Channel("anime","a"),new Channel("music","m"),new Channel("random","b"), new Channel("anime","a"),new Channel("music","m")];
@@ -98,13 +95,13 @@ class Post {
         this.comments = comments;
     }
     public AddComment(txt: string) {
-        console.log("clicked_" + txt);
         this.comments.push(new Comment(0, 0, txt));
         this.typedComment = "";
     }
     public AddTypedComment() {
         if (this.typedComment.trim() === "") return;
-        
+        socket.emit("send-comment",{text:this.typedComment,postId:this.id,channel:"/"});
+        this.typedComment="";
     }
     public ButtonPressed($event: any) {
         if ($event.which === 13) {
