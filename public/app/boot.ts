ce8f762424b8238e64cafbcbc9a16cfb67ec790d
Kwoth <srki0509@gmail.com>
2016-01-01 15:50:43 +0100

    Dropzone include, prod mode, fix css, right section

--- public/app/boot.ts
+++ public/app/boot.ts
@@ -1,8 +1,10 @@
-﻿import {bootstrap}    from 'angular2/platform/browser';
+﻿import { enableProdMode} from 'angular2/core';
+import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component.js';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {SocketService} from './services/socket.service.js';
 
+enableProdMode();
 bootstrap(AppComponent,[
     ROUTER_PROVIDERS,
     SocketService
